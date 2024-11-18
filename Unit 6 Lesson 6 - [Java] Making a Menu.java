51a42ca412e0aa3d97735eaa23dee40ec74197f0
Elliot796834 <796834@pdsb.net>
2024-11-18 12:42:13 -0500

    Reflection

--- Unit 6 Lesson 6 - [Java] Making a Menu.java
+++ Unit 6 Lesson 6 - [Java] Making a Menu.java
@@ -1,16 +1,1 @@
-import java.io.*;
-import java.util.Scanner;
 
-class menu {
-  public static void main(String [] args) {
-  	Scanner kb = new Scanner(System.in);
-  	int choice = 0;
-  	boolean valid = false;
-  	while (! valid) {
-  		System.out.print("Choose a, b or c: ");
-  		choice = kb.nextInt();
-  		valid = ((choice >= 1) && (choice <= 3));
-  	}
-  	System.out.printf("You chose: %d\n", choice);
-  }
-}
