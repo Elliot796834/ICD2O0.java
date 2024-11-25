8266ebd0b553dcc766c87763e08215f903dd0a1c
Elliot796834 <796834@pdsb.net>
2024-11-25 12:55:41 -0500

    Create Unit 6 Lesson 12 - Debug Loops Part A: debug a conventional loop.java

---
+++ Unit 6 Lesson 12 - Debug Loops Part A: debug a conventional loop.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 12; i++) {
+            double sqrtValue = Math.sqrt(i);
+            System.out.printf("%d %.4f%n", i, sqrtValue);
+        }
+    }
+}
